1f083def65c5f88870051f7870b4db7bf907aa97
Pablo Fernandez <pablo_fz@hotmail.com>
2020-12-30 14:30:35 +0100

    terminados controladores y rutas para signIn y LogIn

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const app = express();
 const logger = require("morgan");
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
+const connectDb = require("./configs/db.config");
+const router = require("./routes/routes");
+const connectSession = require("./configs/session.config");
 
 hbs.registerPartials(__dirname + "/views/partials");
+
+connectDb();
+connectSession(app);
 
 app.set("views",`${__dirname}/views`);
 app.set("view engine", "hbs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(`${__dirname}/public`));
+app.use("/", router );
 
 app.listen(process.env.PORT,() => {
     console.log(`Listening on http://localhost:${process.env.PORT}`);}); 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,3 +13,76 @@ const isMongooseValidationError = (error) =>
 const isMongoError = ({ code: errorCode }) => errorCode === 11000;
 
 
+const signIn = async (req, res, next) => {
+    try {
+      const { user, password, email } = req.body;
+      //console.log(user,password,email);
+      const missingCredentials = !password || !email || !user;
+      if (missingCredentials) {
+        return res.send("missing credentials");
+      }
+      if (!hasCorrectPassword(password)) {
+        return res.send("incorrect password format");
+      }
+  
+      const usuario = User.findOne({ username: user }).lean();
+      if (!usuario) {
+        return res.send("user already exist");
+      }
+  
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      const hashedPassword = await bcrypt.hash(password, salt);
+  
+      const { passwordHash, ...rest } = await User.create({
+        email,
+        passwordHash: hashedPassword,
+        username: user,
+      });
+      //console.log("user", rest);
+      res.send("aquí saldrían las 6 cartas por ser el primer registro, las añadiríamos al usuario y agragaríamos el usuario a la sesión");
+    } catch (err) {
+      if (isMongooseValidationError(err)) {
+        console.error(err);
+        return res.send("validation error: " + err.message);
+      }
+  
+      if (isMongoError(err)) {
+        return res.send("mongo error: " + err.message);
+      }
+  
+      console.error(err);
+    }
+  };
+
+  
+const logIn = async (req, res, next) => {
+    try {
+      const { user, password } = req.body;
+      const missingCredentials = !password || !user;
+      if (missingCredentials) {
+        return res.send("missing credentials");
+      }
+      const { passwordHash, ...usuario } = await User.findOne({ username:user }).lean();
+      if (!usuario) {
+        return res.send("user does not exist");
+      }
+  
+      const verifiedPassword = await bcrypt.compare(password,passwordHash);
+      if (!verifiedPassword) {
+          return res.send("invalid credentials");
+        }
+        console.log(usuario);
+        console.log(req.session);
+        req.session.currentUser = usuario;
+        res.redirect("/homepage");
+  
+  
+      
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  module.exports = { logIn, signIn};
